1f1684557129f543f6606689dc1d2129e240e90e
Romain Pouclet <romain.pouclet@gmail.com>
2017-10-23 15:58:27 -0700

    Crash that shit

--- src/auth/screens/login.screen.js
+++ src/auth/screens/login.screen.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-shadow */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import {
   ActivityIndicator,
   View,
@@ -11,18 +11,18 @@ import {
   Image,
   WebView,
   Platform,
-  Modal,
-} from 'react-native';
-import { Button, Icon } from 'react-native-elements';
-import Swiper from 'react-native-swiper';
-import queryString from 'query-string';
-import CookieManager from 'react-native-cookies';
+  Modal
+} from "react-native";
+import { Button, Icon } from "react-native-elements";
+import Swiper from "react-native-swiper";
+import queryString from "query-string";
+import CookieManager from "react-native-cookies";
 
-import { ViewContainer } from 'components';
-import { colors, fonts, normalize } from 'config';
-import { CLIENT_ID } from 'api';
-import { auth, getUser } from 'auth';
-import { translate, resetNavigationTo } from 'utils';
+import { ViewContainer } from "components";
+import { colors, fonts, normalize } from "config";
+import { CLIENT_ID } from "api";
+import { auth, getUser } from "auth";
+import { translate, resetNavigationTo } from "utils";
 
 let stateRandom = Math.random().toString();
 
@@ -30,14 +30,14 @@ const mapStateToProps = state => ({
   locale: state.auth.locale,
   isLoggingIn: state.auth.isLoggingIn,
   isAuthenticated: state.auth.isAuthenticated,
-  hasInitialUser: state.auth.hasInitialUser,
+  hasInitialUser: state.auth.hasInitialUser
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
       auth,
-      getUser,
+      getUser
     },
     dispatch
   );
@@ -45,8 +45,8 @@ const mapDispatchToProps = dispatch =>
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   button: {
     backgroundColor: colors.transparent,
@@ -55,99 +55,99 @@ const styles = StyleSheet.create({
     borderColor: colors.white,
     paddingVertical: 10,
     paddingHorizontal: 30,
-    shadowColor: colors.transparent,
+    shadowColor: colors.transparent
   },
   buttonContainer: {
-    backgroundColor: colors.transparent,
+    backgroundColor: colors.transparent
   },
   buttonText: {
     ...fonts.fontPrimaryBold,
-    fontSize: normalize(12),
+    fontSize: normalize(12)
   },
   browserDismiss: {
-    flex: 1,
+    flex: 1
   },
   browserLoader: {
     flex: 1,
-    position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%',
-    textAlign: 'center',
-    backgroundColor: colors.githubDark,
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    textAlign: "center",
+    backgroundColor: colors.githubDark
   },
   browserLoadingLabel: {
     fontSize: normalize(20),
     color: colors.white,
     ...fonts.fontPrimarySemiBold,
-    paddingBottom: 20,
+    paddingBottom: 20
   },
   browserSection: {
-    flex: 5,
+    flex: 5
   },
   logo: {
     width: 90,
-    height: 90,
+    height: 90
   },
   miniSection: {
     flex: 1.5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   modalContainer: {
     flex: 1,
     paddingTop: 20,
-    backgroundColor: '#1f2327',
+    backgroundColor: "#1f2327"
   },
   contentSection: {
     flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   signInContainer: {
-    position: 'absolute',
+    position: "absolute",
     right: 0,
     left: 0,
     bottom: 80,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   slide: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 50
   },
   slide1: {
-    backgroundColor: colors.lightBlue,
+    backgroundColor: colors.lightBlue
   },
   slide2: {
-    backgroundColor: colors.lightPurple,
+    backgroundColor: colors.lightPurple
   },
   slide3: {
-    backgroundColor: colors.orange,
+    backgroundColor: colors.orange
   },
   slide4: {
-    backgroundColor: colors.darkGreen,
+    backgroundColor: colors.darkGreen
   },
   title: {
     fontSize: normalize(20),
-    textAlign: 'center',
+    textAlign: "center",
     color: colors.white,
     ...fonts.fontPrimarySemiBold,
     marginTop: 45,
-    marginBottom: 15,
+    marginBottom: 15
   },
   message: {
     fontSize: normalize(14),
-    textAlign: 'center',
+    textAlign: "center",
     color: colors.white,
-    ...fonts.fontPrimaryLight,
+    ...fonts.fontPrimaryLight
   },
   iconMargin: {
-    marginLeft: 20,
-  },
+    marginLeft: 20
+  }
 });
 
 class Login extends Component {
@@ -158,7 +158,7 @@ class Login extends Component {
     locale: string,
     auth: Function,
     getUser: Function,
-    navigation: Object,
+    navigation: Object
   };
 
   constructor() {
@@ -169,21 +169,21 @@ class Login extends Component {
       modalVisible: false,
       cancelDisabled: false,
       showLoader: true,
-      loaderText: translate('auth.login.connectingToGitHub', this.locale),
-      asyncStorageChecked: false,
+      loaderText: translate("auth.login.connectingToGitHub", this.locale),
+      asyncStorageChecked: false
     };
   }
 
   componentDidMount() {
     if (this.props.isAuthenticated) {
-      this.props.navigation.navigate('Main');
+      this.props.navigation.navigate("Main");
     } else {
       // FIXME
       // eslint-disable-next-line react/no-did-mount-set-state
       this.setState({ asyncStorageChecked: true });
 
-      if (Platform.OS === 'android') {
-        Linking.addEventListener('url', this.handleOpenURL);
+      if (Platform.OS === "android") {
+        Linking.addEventListener("url", this.handleOpenURL);
         Linking.getInitialURL().then(url => {
           if (url) {
             this.handleOpenURL({ url });
@@ -194,8 +194,8 @@ class Login extends Component {
   }
 
   componentWillUnmount() {
-    if (Platform.OS === 'android') {
-      Linking.removeEventListener('url', this.handleOpenURL);
+    if (Platform.OS === "android") {
+      Linking.removeEventListener("url", this.handleOpenURL);
     }
   }
 
@@ -206,19 +206,20 @@ class Login extends Component {
   };
 
   setModalVisible = visible => {
+    undefined["potato"]();
     this.setState({ modalVisible: visible });
   };
 
   toggleCancelButton = (e, disabled) => {
     const url = e.nativeEvent.url;
 
-    if (url === 'https://github.com/session') {
+    if (url === "https://github.com/session") {
       this.setState({ cancelDisabled: disabled });
     }
   };
 
   handleOpenURL = ({ url }) => {
-    if (url && url.substring(0, 11) === 'gitpoint://') {
+    if (url && url.substring(0, 11) === "gitpoint://") {
       const [, queryStringFromUrl] = url.match(/\?(.*)/);
       const { state, code } = queryString.parse(queryStringFromUrl);
       const { auth, getUser, navigation } = this.props;
@@ -227,7 +228,7 @@ class Login extends Component {
         this.setState({
           code,
           showLoader: true,
-          loaderText: translate('auth.login.preparingGitPoint', this.locale),
+          loaderText: translate("auth.login.preparingGitPoint", this.locale)
         });
 
         stateRandom = Math.random().toString();
@@ -235,7 +236,7 @@ class Login extends Component {
         CookieManager.clearAll().then(() => {
           auth(code, state).then(() => {
             getUser().then(() => {
-              resetNavigationTo('Main', navigation);
+              resetNavigationTo("Main", navigation);
             });
           });
         });
@@ -252,7 +253,9 @@ class Login extends Component {
   renderLoading() {
     return (
       <View style={styles.browserLoader}>
-        <Text style={styles.browserLoadingLabel}>{this.state.loaderText}</Text>
+        <Text style={styles.browserLoadingLabel}>
+          {this.state.loaderText}
+        </Text>
         <ActivityIndicator color={colors.white} size="large" />
       </View>
     );
@@ -263,7 +266,7 @@ class Login extends Component {
 
     return (
       <ViewContainer barColor="light">
-        {this.shouldDisplayIntro() && (
+        {this.shouldDisplayIntro() &&
           <Swiper
             loop={false}
             dotColor="#FFFFFF55"
@@ -272,13 +275,13 @@ class Login extends Component {
             <View style={[styles.slide, styles.slide1]}>
               <Image
                 style={styles.logo}
-                source={require('../../assets/logo.png')}
+                source={require("../../assets/logo.png")}
               />
               <Text style={styles.title}>
-                {translate('auth.login.welcomeTitle', locale)}
+                {translate("auth.login.welcomeTitle", locale)}
               </Text>
               <Text style={styles.message}>
-                {translate('auth.login.welcomeMessage', locale)}
+                {translate("auth.login.welcomeMessage", locale)}
               </Text>
             </View>
             <View style={[styles.slide, styles.slide2]}>
@@ -290,10 +293,10 @@ class Login extends Component {
                 type="octicon"
               />
               <Text style={styles.title}>
-                {translate('auth.login.notificationsTitle', locale)}
+                {translate("auth.login.notificationsTitle", locale)}
               </Text>
               <Text style={styles.message}>
-                {translate('auth.login.notificationsMessage', locale)}
+                {translate("auth.login.notificationsMessage", locale)}
               </Text>
             </View>
             <View style={[styles.slide, styles.slide3]}>
@@ -306,10 +309,10 @@ class Login extends Component {
                 type="octicon"
               />
               <Text style={styles.title}>
-                {translate('auth.login.reposTitle', locale)}
+                {translate("auth.login.reposTitle", locale)}
               </Text>
               <Text style={styles.message}>
-                {translate('auth.login.reposMessage', locale)}
+                {translate("auth.login.reposMessage", locale)}
               </Text>
             </View>
             <View style={[styles.slide, styles.slide4]}>
@@ -322,27 +325,25 @@ class Login extends Component {
                 type="octicon"
               />
               <Text style={styles.title}>
-                {translate('auth.login.issuesTitle', locale)}
+                {translate("auth.login.issuesTitle", locale)}
               </Text>
               <Text style={styles.message}>
-                {translate('auth.login.issuesMessage', locale)}
+                {translate("auth.login.issuesMessage", locale)}
               </Text>
             </View>
-          </Swiper>
-        )}
-        {this.shouldDisplayIntro() && (
+          </Swiper>}
+        {this.shouldDisplayIntro() &&
           <View style={styles.signInContainer}>
             <Button
               raised
-              title={translate('auth.login.signInButton', locale)}
+              title={translate("auth.login.signInButton", locale)}
               containerViewStyle={styles.buttonContainer}
               buttonStyle={styles.button}
               textStyle={styles.buttonText}
               onPress={() => this.setModalVisible(true)}
             />
-          </View>
-        )}
-        {this.shouldDisplayIntro() && (
+          </View>}
+        {this.shouldDisplayIntro() &&
           <Modal
             animationType="slide"
             onRequestClose={() => null}
@@ -353,7 +354,7 @@ class Login extends Component {
               <View style={styles.browserSection}>
                 <WebView
                   source={{
-                    uri: `https://github.com/login/oauth/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=gitpoint://welcome&scope=user%20repo&state=${stateRandom}`,
+                    uri: `https://github.com/login/oauth/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=gitpoint://welcome&scope=user%20repo&state=${stateRandom}`
                   }}
                   onLoadStart={e => this.toggleCancelButton(e, true)}
                   onLoadEnd={e => this.toggleCancelButton(e, false)}
@@ -364,7 +365,7 @@ class Login extends Component {
               </View>
               <View style={styles.miniSection}>
                 <Button
-                  title={translate('auth.login.cancel', locale)}
+                  title={translate("auth.login.cancel", locale)}
                   buttonStyle={styles.button}
                   disabled={this.state.cancelDisabled}
                   textStyle={styles.buttonText}
@@ -372,21 +373,19 @@ class Login extends Component {
                 />
               </View>
             </View>
-          </Modal>
-        )}
+          </Modal>}
         {isLoggingIn &&
-          !hasInitialUser && (
-            <View style={styles.browserLoader}>
-              <Text style={styles.browserLoadingLabel}>
-                {this.state.loaderText}
-              </Text>
-              <ActivityIndicator
-                animating={isLoggingIn}
-                color={colors.white}
-                size="large"
-              />
-            </View>
-          )}
+          !hasInitialUser &&
+          <View style={styles.browserLoader}>
+            <Text style={styles.browserLoadingLabel}>
+              {this.state.loaderText}
+            </Text>
+            <ActivityIndicator
+              animating={isLoggingIn}
+              color={colors.white}
+              size="large"
+            />
+          </View>}
       </ViewContainer>
     );
   }
